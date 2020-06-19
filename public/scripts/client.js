69d6c293187575ff1797b16955742c413b67981a
briantran98 <98brian.tran@gmail.com>
2020-06-19 02:01:17 +0000

    Added scroll animation to write a new tweet button to scroll and focus tweet-text text area

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,7 +12,9 @@ $(document).ready(function () {
   };
 
   const createTweetElement = (tweet) => {
-    const { user, content } = tweet;
+    const { user, content, created_at } = tweet;
+    const elapsedDays = calculateDays(created_at)
+    const safeText = escape(content.text);
     const $tweet = $(
       `<article class='tweet'>
         <header>
@@ -23,14 +25,14 @@ $(document).ready(function () {
           <span>${user.handle}</span>
         </header>
         <div class='tweet-body'>
-          <p>${escape(content.text)}</p>
+          <p>${safeText}</p>
         </div>
         <footer>
-          <p>10 days</p>
+          <p>${elapsedDays}</p>
           <div>
-            <a>a</a>
-            <a>b</a>
-            <a>c</a>
+            <i class="button fas fa-flag"></i>
+            <i class="button fas fa-retweet"></i>
+            <i class="button fas fa-heart"></i>
           </div>
         </footer>
       </article>`
@@ -53,6 +55,30 @@ $(document).ready(function () {
     });
   };
 
+  const calculateDays = (date) => {
+    const currentDate = Date.now()
+    const oneDay = 1000 * 60 * 60 * 24
+    const elapsedTime = (currentDate - date)
+    let day, hour, minute, seconds;
+    seconds = Math.floor(elapsedTime / 1000);
+    minute = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    hour = Math.floor(minute / 60);
+    minute = minute % 60;
+    day = Math.floor(hour / 24);
+    hour = hour % 24;
+    console.log(day);
+    if (day > 0) {
+      return `${day} Days`
+    }
+    if (hour > 0) {
+      return `${hour} Hours`
+    }
+    if (minute >= 0) {
+      return `${minute} Minutes`
+    }
+  }
+
   loadTweets();
 
   $("form").on("submit", function (event) {
@@ -70,5 +96,15 @@ $(document).ready(function () {
         $("#tweet-text").focus();
       });
     }
+  });
+
+  function scrollToAnchor(aid) {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 'slow');
+  }
+
+  $("#new-tweet-link a").click(function() {
+    scrollToAnchor('tweet-text');
   });
 });
